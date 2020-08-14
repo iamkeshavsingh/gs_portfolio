3a124aed68ddcc5673065bcbafe0e5feea1d162a
Keshav Singh Yadav <keshavy561@gmail.com>
2020-08-14 17:59:17 +0530

    increment and decrement functions added but coercion remaining

---
+++ main.js
@@ -0,0 +1,26 @@
+var incButton = document.querySelector('.increment');
+var decButton = document.querySelector('.decrement');
+var counter = document.querySelector('.counter_value');
+
+function setCounterValue(num) {
+    var currentValue = counter.textContent;
+    if (num > 0) {
+        currentValue += 1;
+    }
+    else {
+        currentValue -= 1;
+    }
+    counter.textContent = currentValue;
+}
+
+function increment() {
+    setCounterValue(1);
+}
+
+function decrement() {
+    setCounterValue(-1);
+}
+
+
+incButton.addEventListener('click', increment);
+decButton.addEventListener('click', decrement)
