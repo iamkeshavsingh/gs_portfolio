3a2500a23bc84f51e3603657f72705200e4dc6bb
Keshav Singh Yadav <keshavy561@gmail.com>
2020-08-16 12:35:22 +0530

    empty todo message added

---
+++ main.js
@@ -0,0 +1,10 @@
+var todos = [];
+
+var todo_list_wrapper = document.querySelector('.show_todo');
+
+function updateTodoList(todos) {
+    if (todos.length == 0) {
+    }
+}
+
+updateTodoList(todos);
